2afa125b5391747665795ab86190cc0219e3d391
nedim yesilcinar <yesilcinarnedim@gmail.com>
2025-01-20 15:11:50 +0300

    5

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { StyleSheet, View, Text, TouchableOpacity, Pressable } from "react-native";
+
+
+export default function HomeScreen() {
+
+
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 20,
+    alignItems: 'center',
+  },
+});
